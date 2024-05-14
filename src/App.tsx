0b57c94c06e32ebed6a42d0fe8e2e7e719f20f25
Chris Bolin <bolin.chris@gmail.com>
2024-05-14 17:19:09 -0600

    Grid class

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,50 @@ export function identity(x: any) {
   return x;
 }
 
+type NullableNumber = number | null;
+type GridData<T> = Array<Array<T>>;
+// [
+//   [1,2],
+//   [3,4],
+// ]
+// Grid[yIndex][xIndex]
+
+class Grid<T> {
+  data: GridData<T>;
+
+  constructor(data: GridData<T>) {
+    this.data = data;
+  }
+
+  get(xIndex: number, yIndex: number) {
+    const row = this.data[yIndex] || [];
+    return row[xIndex];
+  }
+
+  ensureSize(xMin: number, yMin: number) {
+    // first check number rows with yMin
+    // then check contents of rows with xMin
+  }
+}
+
 function range(length: number) {
   return new Array(length)
     .fill(0)
     .map((_, index) => index);
 }
 
-function Box({ color, xIndex, yIndex }: { color: string, yIndex: number, xIndex: number }) {
+function randomInt(upperBoundExclusive: number) {
+  return Math.floor(Math.random() * upperBoundExclusive);
+}
+
+function randomGrid(x: number, y: number, max: number) {
+  const data = range(y).map(() => range(x).map(() => randomInt(max)));
+  return new Grid(data);
+}
+
+function Box({ value, xIndex, yIndex }: { value: NullableNumber, yIndex: number, xIndex: number }) {
+  const colors = ['yellow', 'red', 'blue'];
+  const color = value !== null ? colors[value] : 'lightgrey'; // backup color
   const onClick = () => console.log([xIndex, yIndex], color);
   const baseDimension = 10;
   const oversizeDimension = baseDimension + 1; // oversize the width and height to fill small gap in browser rendering
@@ -24,33 +61,26 @@ function Box({ color, xIndex, yIndex }: { color: string, yIndex: number, xIndex:
   />;
 }
 
-function BoxRow({ colors, yIndex }: { colors: Array<string>, yIndex: number }) {
+function BoxRow({ row, yIndex }: { row: Array<NullableNumber>, yIndex: number }) {
   return <>
-    {colors.map((color, index) => <Box color={color} yIndex={yIndex} xIndex={index} key={index} />)}
+    {row.map((value, index) => <Box value={value} yIndex={yIndex} xIndex={index} key={index} />)}
   </>
 }
 
-function BoxGrid({ colorsGrid }: { colorsGrid: Array<Array<string>> }) {
-  const sideLengthInBoxes = 4;
-  const sideLengthInSvgUnits = sideLengthInBoxes * 10;
-  return <svg viewBox={`0 0 ${sideLengthInSvgUnits} ${sideLengthInSvgUnits}`}>
-    {colorsGrid.map((colorsRow, yIndex) => <BoxRow colors={colorsRow} yIndex={yIndex} key={yIndex} />)}
+function BoxGrid({ colorsGrid }: { colorsGrid: GridData<NullableNumber> }) {
+  const widthInBoxes = 6;
+  const heightInBoxes = 10;
+  const pixelsPerBox = 10;
+  return <svg viewBox={`0 0 ${widthInBoxes * pixelsPerBox} ${heightInBoxes * pixelsPerBox}`}>
+    {colorsGrid.map((row, yIndex) => <BoxRow row={row} yIndex={yIndex} key={yIndex} />)}
   </svg>
 }
 
 function App() {
-  console.log(range(10));
+  const grid = randomGrid(6, 10, 3);
   return (
     <div className="App">
-      <BoxGrid colorsGrid={[
-        ["red", "yellow", "blue"],
-        ["red", "red", "blue"],
-        ["yellow", "red", "blue"],
-        ["red", "yellow", "yellow"],
-        ["red", "red", "blue"],
-        ["yellow", "red", "blue"],
-        ["red", "red", "blue"],
-      ]} />
+      <BoxGrid colorsGrid={grid.data} />
     </div>
   );
 }
