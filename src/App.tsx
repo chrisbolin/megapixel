9075511398753f1a77034e56ae56a92d98c23450
Chris Bolin <bolin.chris@gmail.com>
2024-05-14 18:05:47 -0600

    first drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './App.css';
 
 export function identity(x: any) {
@@ -5,23 +7,27 @@ export function identity(x: any) {
 }
 
 type NullableNumber = number | null;
-type GridData<T> = Array<Array<T>>;
+type GridData = Array<Array<NullableNumber>>;
 // [
 //   [1,2],
 //   [3,4],
 // ]
 // Grid[yIndex][xIndex]
 
-class Grid<T> {
-  data: GridData<T>;
+class Grid {
+  data: GridData;
 
-  constructor(data: GridData<T>) {
+  constructor(data: GridData) {
     this.data = data;
   }
 
   get(xIndex: number, yIndex: number) {
     const row = this.data[yIndex] || [];
     return row[xIndex];
+  }
+
+  set(xIndex: number, yIndex: number, value: number) {
+    this.data[yIndex][xIndex] = value;
   }
 
   ensureSize(xMin: number, yMin: number) {
@@ -48,7 +54,6 @@ function randomGrid(x: number, y: number, max: number) {
 function Box({ value, xIndex, yIndex }: { value: NullableNumber, yIndex: number, xIndex: number }) {
   const colors = ['yellow', 'red', 'blue'];
   const color = value !== null ? colors[value] : 'lightgrey'; // backup color
-  const onClick = () => console.log([xIndex, yIndex], color);
   const baseDimension = 10;
   const oversizeDimension = baseDimension + 1; // oversize the width and height to fill small gap in browser rendering
   return <rect
@@ -57,7 +62,6 @@ function Box({ value, xIndex, yIndex }: { value: NullableNumber, yIndex: number,
     fill={color}
     x={xIndex * 10}
     y={yIndex * 10}
-    onClick={onClick}
   />;
 }
 
@@ -67,20 +71,55 @@ function BoxRow({ row, yIndex }: { row: Array<NullableNumber>, yIndex: number })
   </>
 }
 
-function BoxGrid({ colorsGrid }: { colorsGrid: GridData<NullableNumber> }) {
+function BoxGrid({ grid }: { grid: Grid }) {
+  const [count, setCount] = React.useState(0);
+
   const widthInBoxes = 6;
   const heightInBoxes = 10;
   const pixelsPerBox = 10;
-  return <svg viewBox={`0 0 ${widthInBoxes * pixelsPerBox} ${heightInBoxes * pixelsPerBox}`}>
-    {colorsGrid.map((row, yIndex) => <BoxRow row={row} yIndex={yIndex} key={yIndex} />)}
-  </svg>
+  const viewBox = `0 0 ${widthInBoxes * pixelsPerBox} ${heightInBoxes * pixelsPerBox}`;
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  function getBoxFromEvent(grid: Grid, event: React.TouchEvent<SVGElement>) {
+    const { clientX, clientY, target } = event.touches[0];
+    if (target instanceof Element && target.parentElement) {
+      const x = Math.floor(clientX / target.parentElement.clientWidth * widthInBoxes);
+      const y = Math.floor(clientY / target.parentElement.clientHeight * heightInBoxes);
+      const value = grid.get(x, y);
+      return { x, y, value };
+    }
+    return {};
+  }
+
+  function handleTouchStart(event: React.TouchEvent<SVGElement>) {
+
+  }
+
+  function handleTouchMove(event: React.TouchEvent<SVGElement>) {
+    const { x, y, value } = getBoxFromEvent(grid, event);
+
+    if (value === undefined) return;
+    grid.set(x, y, 3);
+    increaseCount();
+    console.log({ x, y, value });
+  }
+
+  return <svg
+    viewBox={viewBox}
+    onTouchMove={handleTouchMove}
+  >
+    {grid.data.map((row, yIndex) => <BoxRow row={row} yIndex={yIndex} key={yIndex} />)}
+  </svg >
 }
 
 function App() {
   const grid = randomGrid(6, 10, 3);
   return (
     <div className="App">
-      <BoxGrid colorsGrid={grid.data} />
+      <BoxGrid grid={grid} />
     </div>
   );
 }
